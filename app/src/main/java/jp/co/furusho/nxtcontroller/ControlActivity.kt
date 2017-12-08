431309c44966cf51eae681810183dc7b482bc62b
Hiroki Furusho <furusityofu+fork@gmail.com>
2017-12-08 12:49:07 +0900

    BluetoothAdapterを追加

--- app/src/main/java/jp/co/furusho/nxtcontroller/ControlActivity.kt
+++ app/src/main/java/jp/co/furusho/nxtcontroller/ControlActivity.kt
@@ -1,16 +1,17 @@
 package jp.co.furusho.nxtcontroller
 
 import android.bluetooth.BluetoothAdapter
+import android.content.Context
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 
 import kotlinx.android.synthetic.main.activity_control.*
 import kotlinx.android.synthetic.main.fragment_control.*
 
 class ControlActivity : AppCompatActivity() {
 
-    var mBluetoothAdapter:BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +24,19 @@ class ControlActivity : AppCompatActivity() {
                     .setAction("Action", null).show()
         }
 
+        var mBluetoothAdapter:BluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+
+        if (mBluetoothAdapter.isEnabled){
+
+            applicationContext.toast("Bluetoothは有効です")
+        }else{
+            applicationContext.toast("Bluetoothは無効です")
+        }
+
+
     }
+
+    fun Context.toast(message: CharSequence) =
+            Toast.makeText(this, message,Toast.LENGTH_SHORT).show()
 
 }
